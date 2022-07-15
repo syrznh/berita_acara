85820ac957b6734b60f4330d41af239e8c4cf231
syrznh <saruriansyah@gmail.com>
2022-07-15 20:19:07 +0700

    update_layout_master

--- app/Views/dashboard/layouts/master.php
+++ app/Views/dashboard/layouts/master.php
@@ -221,17 +221,6 @@
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                                <?php if (session()->get('role') == 1) { ?>
-                                    <a class="dropdown-item" href="#">
-                                        <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                        Profile
-                                    </a>
-                                    <a class="dropdown-item" href="#">
-                                        <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                        Settings
-                                    </a>
-                                <?php } else { ?>
-                                <?php } ?>
                                 <a class="dropdown-item" href="<?php echo base_url('/logout') ?>">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Logout
